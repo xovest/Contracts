b2b13c358592ebb7866e6174595c70500c90acc8
xovest <79008917+xovest@users.noreply.github.com>
2022-01-14 15:26:54 +0200

    simple fns

---
+++ basicFuncs.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.7.1 <0.9.0;
+
+function sum(uint[] memory _arr) pure returns (uint s) {
+    for (uint i = 0; i < _arr.length; i++)
+        s += _arr[i];
+}
+
+contract ArrayExample {
+    bool found;
+    function f(uint[] memory _arr) public {
+        // This calls the free function internally.
+        // The compiler will add its code to the contract.
+        uint s = sum(_arr);
+        require(s >= 10);
+        found = true;
+    }
+}
+
+contract Simple {
+    function arithmetic(uint _a, uint _b)
+        public
+        pure
+        returns (uint o_sum, uint o_product)
+    {
+        return (_a + _b, _a * _b);
+    }
+}
